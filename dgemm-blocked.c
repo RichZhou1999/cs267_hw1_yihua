9101c0e92d631c84efcc592577d34a7c19fdd250
RichZhou1999 <373072096@qq.com>
2023-02-01 11:46:20 -0800

    1

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -3,7 +3,7 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE 128
+#define BLOCK_SIZE 512
 #endif
 #ifndef BLOCK_SIZE_INNER
 #define BLOCK_SIZE_INNER 256
